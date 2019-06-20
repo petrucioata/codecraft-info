c9df38aa6ddf48882d9e382406d2b809b3a7d289
Petru Cioata <petru.cioata@assist.ro>
2019-06-20 21:28:03 +0300

    Set configuration for dev/test databases

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module CodecraftInfo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.before_configuration do
+      local_env = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(local_env)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(local_env)
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
