3482c7e41597f32838ef1e1e5d8dab69f6f72597
Petru Cioata <petru.cioata@assist.ro>
2019-06-22 19:09:12 +0300

    Update position and its spec

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,3 +1,4 @@
 class Position < ApplicationRecord
   validates_uniqueness_of :short_name
+  validates_presence_of :short_name, :long_name
 end
--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Position, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:position) { described_class.new(short_name: short_name, long_name: long_name) }
+
+  context 'when short_name or long_name are missing' do
+    let(:short_name) { nil }
+    let(:long_name) { nil }
+
+    it 'is invalid without the short_name' do
+      expect(subject).to_not be_valid
+    end
+    it 'is invalid without the long_name' do
+      expect(subject).to_not be_valid
+    end
+  end
+
+  context 'when short_name, long_name are provided' do
+    let(:short_name) { 'SDE test' }
+    let(:long_name) { 'Software Development Engineer test' }
+
+    it 'is valid with short_name' do
+      expect(subject).to be_valid
+    end
+
+    it 'is valid with long_name' do
+      expect(subject).to be_valid
+    end
+  end
 end
