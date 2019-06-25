636a68342cdf41a614d6e6deb7f322d41cf63194
Petru Cioata <petru.cioata@assist.ro>
2019-06-25 19:15:26 +0300

    Add views and controller methods for Edition

--- app/controllers/editions_controller.rb
+++ app/controllers/editions_controller.rb
@@ -1,17 +1,47 @@
 class EditionsController < ApplicationController
-  before_action :set_edition, only: [:show, :edit, :delete]
+  before_action :set_edition, only: [:show, :edit, :update, :delete]
 
+  # GET /editions
   def index
     @editions = Edition.all
   end
 
+  # GET /editions/:id
   def show
   end
 
+  # GET /editions/new
+  def new
+    @edition = Edition.new
+  end
+
+  # GET /editions/:id/edit
   def edit
   end
 
+  # POST /editions
+  def create
+    @edition = Edition.new(edition_params)
+
+    if @edition.save
+      redirect_to @edition, notice: 'Edition was succesfully created.'
+    else
+      render action: "new"
+    end
+  end
+
+  # PUT /editions/:id
+  def update
+    if @edition.update_attributes(edition_params)
+      redirect_to @edition, notice: 'Edition was succesfully updated.'
+    else
+      render action: "new"
+    end
+  end
+
   def delete
+    @edition.destroy
+    redirect_to editions_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root "editions#index"
 
-  resources :editions, only: [:index, :show]
-  # get 'editions/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :editions
 end
