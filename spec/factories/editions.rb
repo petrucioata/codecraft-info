22c6c3f5d7026d396bf0f3fa4904f8850812a296
PrelipceanAlexandru <prelipceanalexg@gmail.com>
2019-06-27 12:31:16 +0300

    Edition factory (#1)
    * Add factory bot and create edition factory
    
    * Add Faker and use it in factory

---
+++ spec/factories/editions.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :edition do
+    name { Faker::Name.first_name }
+    topic { Faker::Lorem.sentence }
+    link { "https://#{Faker::Company.name}.com" }
+    description { Faker::Lorem.sentence }
+    date { Faker::Time.between(DateTime.now - 1, DateTime.now) }
+  end
+end
