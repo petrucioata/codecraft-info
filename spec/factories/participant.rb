7274e66e8d1bd868f9420cf5cd6477ebdea892f1
Petru Cioata <petru.cioata@assist.ro>
2019-07-01 14:53:40 +0300

    [T-31163] Update participant factory

--- spec/factories/participant.rb
+++ spec/factories/participant.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :participant do
-    username { Faker::Name.unique.name }
+    username { Faker::Lorem.unique.word }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     position { create(:position) }
