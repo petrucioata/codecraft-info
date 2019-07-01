3007d0eb4247dc2671b57d49401f251524174734
Petru Cioata <petru.cioata@assist.ro>
2019-07-01 15:34:51 +0300

    ParticipantsController and corresponding views (#3)
    * [T-31163] Index and create methods for ParticipantsController
    
    * [T-31163] Update participant factory
    
    * [T-31163] Complete participant's specs and controller methods
    
    * [T-31163] Add and complete participant's views

---
+++ spec/factories/participant.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :participant do
+    username { Faker::Lorem.unique.word }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    position { create(:position) }
+    link { "https://app.codesignal.com/profile/#{username}" }
+  end
+end
