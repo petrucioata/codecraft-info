a017d25dfa98671d01ee4cf56ba1b7ef97c2bb21
Petru Cioata <petru.cioata@assist.ro>
2019-07-03 17:55:58 +0300

    Fix bugs - solution wrong column & select default value

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -4,6 +4,6 @@ class Solution < ApplicationRecord
   belongs_to :participation
   belongs_to :task
 
-  validates :total_points, numericality: { only_integer: true }
+  validates :points, numericality: { only_integer: true }
   validates :participation_id, uniqueness: { scope: :task_id }
 end
