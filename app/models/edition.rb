e2a1804441b53bb57d39e7cb10ac8ba1e945b244
Petru Cioata <petru.cioata@assist.ro>
2019-07-04 13:58:36 +0300

    Implement import_csv for edition

--- app/models/edition.rb
+++ app/models/edition.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'csv'
+
 class Edition < ApplicationRecord
   has_many :participations, dependent: :destroy
   has_many :participants, through: :participations
@@ -12,4 +14,49 @@ class Edition < ApplicationRecord
   validates :name, presence: true
   validates :link, presence: true
   validates :date, presence: true
+
+  def import_csv(file)
+    rows = CSV.read(file.path, headers: true)
+
+    tasks = find_or_create_tasks(tasks_name(rows.headers))
+
+    rows.each do |row|
+      participant = Participant.where(username: row['username']).first_or_create
+      participation = find_or_create_participation(participant, row['points'], row['total_time'])
+      find_or_create_solutions(participation, tasks, row)
+    end
+  end
+
+  private
+
+  def tasks_name(headers)
+    (2..4).to_a.inject([]) { |list, value| list << headers[value] }
+  end
+
+  def find_or_create_tasks(tasks_name)
+    tasks_name.each_with_object([]) do |task_name, tasks|
+      tasks << Task.where(name: task_name, edition: self).first_or_create
+    end
+  end
+
+  def split_solution(result)
+    result.split == 1 ? [0, '00:00:00'] : result.split
+  end
+
+  def find_or_create_participation(participant, points, time)
+    Participation.where(participant: participant, edition: self).first_or_create do |part|
+      part.total_points = points
+      part.total_time = time
+    end
+  end
+
+  def find_or_create_solutions(participation, tasks, row)
+    tasks.each do |task|
+      points, time = split_solution(row[task.name])
+      Solution.where(participation: participation, task: task).first_or_create do |sol|
+        sol.points = points.to_i
+        sol.time = time
+      end
+    end
+  end
 end
--- spec/models/edition_spec.rb
+++ spec/models/edition_spec.rb
@@ -3,5 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe Edition, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:edition) { create(:edition) }
+
+  describe '.import_csv' do
+    before { edition.import_csv(fixture_file_upload('well_formatted.csv')) }
+
+    context 'when the provide CSV is well formatted' do
+      let(:rows) { CSV.read(fixture_file_upload('well_formatted.csv'), headers: true) }
+      let(:usernames) { rows['username'] }
+      let(:headers) { %w[nr_crt username task_a task_b task_c points total_time] }
+
+      it { expect(rows.headers).to eq headers }
+      it { expect(Task.count).to eq 3 }
+      it { expect(Task.all.map(&:name)).to all(include('task_')) }
+
+      it { expect(Participant.count).to eq rows.size }
+      it { expect(Participant.all.map(&:username)).to match usernames }
+
+      it { expect(Participation.count).to eq rows.size }
+      it { expect(Participation.all.map(&:edition_id)).to all eq edition.id }
+
+      it { expect(Solution.count).to eq rows.size * 3 }
+    end
+  end
 end
