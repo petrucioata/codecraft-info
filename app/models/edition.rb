f9bcecb5d3f771408a0ec72b7a1c10f9fcc037de
Petru Cioata <petru.cioata@assist.ro>
2019-07-05 11:09:55 +0300

    Update edition import and removed tasks_name and split_solution private methods

--- app/models/edition.rb
+++ app/models/edition.rb
@@ -17,30 +17,24 @@ class Edition < ApplicationRecord
 
   def import_csv(file)
     rows = CSV.read(file.path, headers: true)
-
-    tasks = find_or_create_tasks(tasks_name(rows.headers))
+    tasks = find_or_create_tasks(rows.headers[2..4])
 
     rows.each do |row|
       participant = Participant.where(username: row['username']).first_or_create
       participation = find_or_create_participation(participant, row['points'], row['total_time'])
       find_or_create_solutions(participation, tasks, row)
     end
+  rescue StandardError => e
+    e.message
+    false
   end
 
   private
-
-  def tasks_name(headers)
-    (2..4).to_a.inject([]) { |list, value| list << headers[value] }
-  end
 
   def find_or_create_tasks(tasks_name)
     tasks_name.each_with_object([]) do |task_name, tasks|
       tasks << Task.where(name: task_name, edition: self).first_or_create
     end
-  end
-
-  def split_solution(result)
-    result.split == 1 ? [0, '00:00:00'] : result.split
   end
 
   def find_or_create_participation(participant, points, time)
@@ -52,7 +46,8 @@ class Edition < ApplicationRecord
 
   def find_or_create_solutions(participation, tasks, row)
     tasks.each do |task|
-      points, time = split_solution(row[task.name])
+      points, time = row[task.name].split == 1 ? [0, '00:00:00'] : row[task.name].split
+
       Solution.where(participation: participation, task: task).first_or_create do |sol|
         sol.points = points.to_i
         sol.time = time
