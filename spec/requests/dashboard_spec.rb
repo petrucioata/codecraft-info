d0a107a7fd76c1369da267d5e2a1e2c00fddbbd2
Petru Cioata <petru.cioata@assist.ro>
2019-07-15 17:47:31 +0300

    [TASK-31369] Rewrite dashboard chart expectations

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -8,16 +8,10 @@ RSpec.describe 'Dashboard', type: :request do
   context 'when dashboard page is displayed' do
     let!(:edition) { create(:edition, :with_participants, count: 5) }
 
-    it 'returns a successful response' do
-      dashboard
+    before { dashboard }
 
-      expect(response).to be_successful
-    end
-
-    it 'display edition name and number of participants' do
-      dashboard
-
-      expect(edition.participants.count).to eq 5
-    end
+    it { expect(response).to be_successful }
+    it { expect(response.body).to include(edition.name) }
+    it { expect(response.body).to include(edition.participants.count.to_s) }
   end
 end
