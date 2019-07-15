30a71fa3941933efd07956942d5fdc859c004780
Alex <prelipceanalexg@gmail.com>
2019-07-15 15:22:26 +0300

    [task-31369] use variable in partials

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class DashboardController < ApplicationController
-  # GET /root
+  # GET /
   def index
     @editions = Edition.all.each_with_object([]) do |edition, data|
-      data << { name: edition.name, nr: edition.participants.count }
+      data << { name: edition.name, number: edition.participants.count }
     end
   end
 end
