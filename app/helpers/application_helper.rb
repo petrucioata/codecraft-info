f47666fc2b14a234f0e087a09b30400d0fe0a439
Petru Cioata <petru.cioata@assist.ro>
2019-07-30 18:52:00 +0300

    Add active_css_class for navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,8 @@ module ApplicationHelper
   def error_messages(error)
     error&.full_messages&.join("\n")
   end
+
+  def active_css_class(link_path)
+    current_page?(link_path) ? 'active' : ''
+  end
 end
