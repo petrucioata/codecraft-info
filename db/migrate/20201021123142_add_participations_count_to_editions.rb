7fbb6b6f110a044cc4aaddd1f87237ea46dd8b9f
Petru G. Cioată <petru.cioata@gmail.com>
2020-10-22 09:31:26 +0300

    Update migration - change to up & down

--- db/migrate/20201021123142_add_participations_count_to_editions.rb
+++ db/migrate/20201021123142_add_participations_count_to_editions.rb
@@ -1,5 +1,13 @@
 class AddParticipationsCountToEditions < ActiveRecord::Migration[5.2]
-  def change
+  def up
     add_column :editions, :participations_count, :integer
+
+    Edition.find_each do |edition|
+      Edition.reset_counters(edition.id, :participations)
+    end
+  end
+
+  def down
+    remove_column :editions, :participations_count
   end
 end
--- db/migrate/20201022051302_populate_edition_participations_count.rb
+++
@@ -1,7 +0,0 @@
-class PopulateEditionParticipationsCount < ActiveRecord::Migration[5.2]
-  def up
-    Edition.find_each do |edition|
-      Edition.reset_counters(edition.id, :participations)
-    end
-  end
-end
