82caa39f86dda951ff77acc3f94e1818295d8be1
AnamariaaNistor <nistoranamaria291@gmail.com>
2020-10-26 14:56:28 +0200

    Added the methods needed for the table

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,9 +5,59 @@ class DashboardController < ApplicationController
   def index
     @participants_per_edition = participants_per_edition
     @implication_level = implication_level
+    @average_points_per_edition = average_points_per_edition
+    @first_five_places = first_places
   end
 
   private
+
+  def first_places
+    Edition.not_deleted.includes(:participations, :participants).each_with_object([]) do |edition, data|
+      data << compute_values(edition)
+    end
+  end
+
+  def first_five_points(edition)
+    edition.participations.first(5).map(&:total_points)
+  end
+
+  def first_five_participants(edition)
+    edition.participants.first(5).map(&:username)
+  end
+
+  def compute_values(edition)
+    points = first_five_points(edition)
+    participants = first_five_participants(edition)
+    {
+      year: edition.month_and_year,
+      first_place_points: points[0],
+      second_place_points: points[1],
+      third_place_points: points[2],
+      fourth_place_points: points[3],
+      fifth_place_points: points[4],
+      first_place_username: participants[0],
+      second_place_username: participants[1],
+      third_place_username: participants[2],
+      fourth_place_username: participants[3],
+      fifth_place_username: participants[4]
+    }
+  end
+
+  def average_points_per_edition
+    Edition.not_deleted.includes(:participations_with_points).each_with_object([]) do |edition, data|
+      # participation_with_points = edition.participations.with_points
+      data << {
+        name: edition.month_and_year,
+        number: calculate_average(edition.participations_with_points)
+      }
+    end
+  end
+
+  def calculate_average(participations)
+    return 0 if participations.size.zero?
+
+    participations.pluck(:total_points).sum / participations.size
+  end
 
   def participants_per_edition
     Participation.all.includes(:edition)
